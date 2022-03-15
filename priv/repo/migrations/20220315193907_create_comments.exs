c66087fd0a1c34a2b01bd3c67c9c301b751b1a7f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-03-15 16:42:43 -0300

    :sparkles: add comments schema and migration

---
+++ priv/repo/migrations/20220315193907_create_comments.exs
@@ -0,0 +1,15 @@
+defmodule Blog.Repo.Migrations.CreateComments do
+  use Ecto.Migration
+
+  def change do
+    create table(:comments, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :body, :text
+      add :post_id, references(:posts, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:comments, [:post_id])
+  end
+end
