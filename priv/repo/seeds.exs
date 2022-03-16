c87c5c908c8fa0bb8b8f53dfbeb7800007644471
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-03-16 09:27:38 -0300

    :seedling: add posts and comments

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,26 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Blog.Repo.insert!(%Blog.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# Primeiro Post
+{:ok, post1} = %Blog.Post{}
+|> Blog.Post.changeset(%{title: "Primeiro Post", body: "Conteúdo do primeiro post"})
+|> Blog.Repo.insert!
+
+%Blog.Comment{}
+|> Blog.Comment.changeset(%{body: "comentário do Primeiro Post", post_id: post1.id})
+|> Blog.Repo.insert!
+
+# Segundo Post
+{:ok, post2} = %Blog.Post{}
+|> Blog.Post.changeset(%{title: "Segundo Post", body: "Conteúdo do segundo post"})
+|> Blog.Repo.insert!
+
+%Blog.Comment{}
+|> Blog.Comment.changeset(%{body: "comentário do Segundo Post", post_id: post2.id})
+|> Blog.Repo.insert!
+
+# Terceiro Post
+{:ok, post3} = %Blog.Post{}
+|> Blog.Post.changeset(%{title: "Terceiro Post", body: "Conteúdo do terceiro post"})
+|> Blog.Repo.insert!
+
+%Blog.Comment{}
+|> Blog.Comment.changeset(%{body: "comentário do Terceiro Post", post_id: post3.id})
+|> Blog.Repo.insert!
