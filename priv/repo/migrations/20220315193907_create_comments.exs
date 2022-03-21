3720dad86e60d6a5208d5b92891c2b9872ade2b6
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-03-21 09:12:28 -0300

    :sparkles: add delete_all

--- priv/repo/migrations/20220315193907_create_comments.exs
+++ priv/repo/migrations/20220315193907_create_comments.exs
@@ -5,7 +5,7 @@ defmodule Blog.Repo.Migrations.CreateComments do
     create table(:comments, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :body, :text
-      add :post_id, references(:posts, on_delete: :nothing, type: :binary_id)
+      add :post_id, references(:posts, on_delete: :delete_all, type: :binary_id)
 
       timestamps()
     end
